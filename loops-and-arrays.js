51230d13a719d657bf41305d053db7951732a13e
sprksld <b.sporkslede@xs4all.nl>
2022-10-17 22:57:35 +0200

    homework done

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -12,8 +12,10 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
 
-
-
+console.log(names);
+for (let i = 0; i < names.length; i++)
+    names[i] += "je";
+console.log(names);
 
 // ==========================================
 // Opdracht 2
@@ -29,8 +31,14 @@ const numbers = [2, 4, 5, 29, 38];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(numbers) geeft: [ 4, 8, 15, 87, 76 ];
 
-
-
+console.log(numbers);
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2)
+        numbers[i] *= 3;
+    else
+        numbers[i] *= 2;
+}
+console.log(numbers);
 
 // ==========================================
 // Opdracht 3
@@ -53,3 +61,9 @@ const squares = [30, 2, 8, 24, 11];
 //   'Het volume van 24 is 13824',
 //   'Het volume van 11 is 1331'
 // ]
+
+console.log(squares);
+for (let i = 0; i < squares.length; i++) {
+    squares[i] = "Het volume van " + squares[i] + " is " + Math.pow(squares[i], 3);
+}
+console.log(squares);
